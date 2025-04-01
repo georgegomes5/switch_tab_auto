aafcf1c45b2800f19700b608e42c3e99a5ef9a3c
georgegomes5 <desmoines89@gmail.com>
2025-04-01 11:14:28 -0400

    Adição dos endereços das página no Power BI

--- script.py
+++ script.py
@@ -6,16 +6,12 @@ driver = webdriver.Edge()
 
 # Lista de URLs para abrir como abas
 urls = [
-    "https://www.google.com",
-    "https://powerbi.microsoft.com",
-    "https://www.bing.com",
-    "https://www.microsoft.com",
-    "https://www.github.com",
-    "https://www.python.org",
-    "https://www.stackoverflow.com",
-    "https://www.amazon.com",
-    "https://www.wikipedia.org",
-    "https://www.facebook.com"
+    "https://app.powerbi.com/groups/me/reports/1fc5b222-b34e-468e-bdfb-6896e58cb6ce/fb8c5ac5d90d9c99a93a?chromeless=true&experience=power-bi",
+    "https://app.powerbi.com/groups/me/reports/1fc5b222-b34e-468e-bdfb-6896e58cb6ce/03ece03b7ec53bd61f4e?chromeless=true&experience=power-bi",
+    "https://app.powerbi.com/groups/me/reports/1fc5b222-b34e-468e-bdfb-6896e58cb6ce/5badfa4e9031d9c33974?chromeless=true&experience=power-bi",
+    "https://app.powerbi.com/groups/me/reports/1fc5b222-b34e-468e-bdfb-6896e58cb6ce/4f15e80ed0b9580481e0?chromeless=true&experience=power-bi",
+    "https://app.powerbi.com/groups/me/reports/1fc5b222-b34e-468e-bdfb-6896e58cb6ce/0cd786648a987de0ddb1?chromeless=true&experience=power-bi",
+    "https://app.powerbi.com/groups/me/reports/1fc5b222-b34e-468e-bdfb-6896e58cb6ce/92f311729b4d2e920cd3?chromeless=true&experience=power-bi"
 ]
 
 # Abre cada URL em uma nova aba
